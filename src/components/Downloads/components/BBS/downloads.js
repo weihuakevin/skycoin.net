d0967c35d2811dfa99ee56c86e3adaf25a450de8
Artour <websheud@gmail.com>
2017-09-27 18:39:17 +1000

    update BBS binaries (#101)

--- src/components/Downloads/components/BBS/downloads.js
+++ src/components/Downloads/components/BBS/downloads.js
@@ -1,4 +1,4 @@
-export const version = 'v3.1.0';
+export const version = 'v4.1.0';
 export const downloads = [{
   platform: 'downloads.wallet.macos',
   icon: 'macos',
@@ -6,9 +6,9 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '',
-      download: 'https://downloads.skycoin.net/bbs/skycoin_bbs_3.1_osx_amd64.zip',
+      download: 'https://downloads.skycoin.net/bbs/skycoin_bbs_4.1_osx_amd64.zip',
       filetype: '.zip',
-      filesize: '5.3mb',
+      filesize: '5.5mb',
     }],
   }],
 }, {
@@ -18,9 +18,9 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '64bit',
-      download: 'https://downloads.skycoin.net/bbs/skycoin_bbs_3.1_windows_amd64.zip',
+      download: 'https://downloads.skycoin.net/bbs/skycoin_bbs_4.1_windows_amd64.zip',
       filetype: '.zip',
-      filesize: '5.3mb',
+      filesize: '5.5mb',
     }],
   }],
 }, {
@@ -30,9 +30,9 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '64bit',
-      download: 'https://downloads.skycoin.net/bbs/skycoin_bbs_3.1_linux_amd64.zip',
+      download: 'https://downloads.skycoin.net/bbs/skycoin_bbs_4.1_linux_amd64.zip',
       filetype: '.zip',
-      filesize: '5.4mb',
+      filesize: '5.5mb',
     }],
   }],
 }];
