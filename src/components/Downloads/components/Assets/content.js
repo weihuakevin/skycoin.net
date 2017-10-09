ff82aaa43db0358a80cf04ed0998b1fec2381f79
gz-c <token@protonmail.com>
2017-10-08 20:10:58 -0500

    Update branding links v1.4 (#122)

--- src/components/Downloads/components/Assets/content.js
+++ src/components/Downloads/components/Assets/content.js
@@ -1,11 +1,11 @@
 export default [{
   name: 'Skycoin Brand Guidelines [pdf]',
-  download: 'https://downloads.skycoin.net/branding/Skycoin-Brand_Guidelines-v1.1.pdf',
+  download: 'https://downloads.skycoin.net/branding/Skycoin-Brand_Guidelines-v1.4.pdf',
   filetype: '.pdf',
   filesize: '2.7mb',
 }, {
   name: 'Skycoin Brand Guidelines [sketch]',
-  download: 'https://downloads.skycoin.net/branding/Skycoin-Brand_Guidelines-v1.1.sketch',
+  download: 'https://downloads.skycoin.net/branding/Skycoin-Brand_Guidelines-v1.4.sketch',
   filetype: '.sketch',
   filesize: '2.9mb',
 }, {
