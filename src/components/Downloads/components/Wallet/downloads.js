1b9c25d0ec2a994e3a593988e70c4358b0db2c4e
gz-c <token@protonmail.com>
2017-10-12 12:18:07 -0500

    Bump wallet to version 0.20.2 (#125)

--- src/components/Downloads/components/Wallet/downloads.js
+++ src/components/Downloads/components/Wallet/downloads.js
@@ -1,4 +1,4 @@
-export const version = 'v0.19.0';
+export const version = 'v0.20.2';
 export const downloads = [{
   platform: 'downloads.wallet.macos',
   icon: 'macos',
@@ -6,8 +6,8 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-gui-osx-x64.dmg',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-gui-osx-x64.dmg',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.dmg',
       filesize: '44mb',
     }],
@@ -15,8 +15,8 @@ export const downloads = [{
     name: 'downloads.wallet.withoutElectron',
     architectures: [{
       name: '',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-osx-darwin-x64.zip',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-osx-darwin-x64.zip',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.zip',
       filesize: '11mb',
     }],
@@ -28,8 +28,8 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-gui-win-setup.exe',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-gui-win-setup.exe',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.exe',
       filesize: '68mb',
     }],
@@ -37,14 +37,14 @@ export const downloads = [{
     name: 'downloads.wallet.withoutElectron',
     architectures: [{
       name: '32bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-win-x86.zip',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-win-x86.zip',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.zip',
       filesize: '11mb',
     }, {
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-win-x64.zip',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-win-x64.zip',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.zip',
       filesize: '11mb',
     }],
@@ -56,23 +56,23 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-gui-linux-x64.AppImage',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-gui-linux-x64.AppImage',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.AppImage',
       filesize: '59mb',
     }],
   }, {
     name: 'downloads.wallet.withoutElectron',
     architectures: [{
-      name: '32bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-linux-arm.tar.gz',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      name: 'arm',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-linux-arm.tar.gz',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.tar.gz',
       filesize: '7.9mb',
     }, {
       name: '64bit',
-      download: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0-bin-linux-x64.tar.gz',
-      torrent: 'http://downloads.skycoin.net/wallet/skycoin-0.19.0.torrent',
+      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-linux-x64.tar.gz',
+      torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.tar.gz',
       filesize: '8.1mb',
     }],
