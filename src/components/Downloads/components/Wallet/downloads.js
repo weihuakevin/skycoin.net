0c251f7411cd77b7629e6559735cf9acc460dd9f
gz-c <token@protonmail.com>
2017-10-13 00:22:24 -0500

    Fix dmg download link; update filesizes (#126)

--- src/components/Downloads/components/Wallet/downloads.js
+++ src/components/Downloads/components/Wallet/downloads.js
@@ -6,10 +6,10 @@ export const downloads = [{
     name: 'downloads.wallet.withElectron',
     architectures: [{
       name: '',
-      download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-gui-osx-x64.dmg',
+      download: 'https://downloads.skycoin.net/wallet/Skycoin-0.20.2.dmg',
       torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.dmg',
-      filesize: '44mb',
+      filesize: '51mb',
     }],
   }, {
     name: 'downloads.wallet.withoutElectron',
@@ -18,7 +18,7 @@ export const downloads = [{
       download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-osx-darwin-x64.zip',
       torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.zip',
-      filesize: '11mb',
+      filesize: '14mb',
     }],
   }],
 }, {
@@ -40,13 +40,13 @@ export const downloads = [{
       download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-win-x86.zip',
       torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.zip',
-      filesize: '11mb',
+      filesize: '14mb',
     }, {
       name: '64bit',
       download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-win-x64.zip',
       torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.zip',
-      filesize: '11mb',
+      filesize: '14mb',
     }],
   }],
 }, {
@@ -59,7 +59,7 @@ export const downloads = [{
       download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-gui-linux-x64.AppImage',
       torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.AppImage',
-      filesize: '59mb',
+      filesize: '56mb',
     }],
   }, {
     name: 'downloads.wallet.withoutElectron',
@@ -68,13 +68,13 @@ export const downloads = [{
       download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-linux-arm.tar.gz',
       torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.tar.gz',
-      filesize: '7.9mb',
+      filesize: '14mb',
     }, {
       name: '64bit',
       download: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2-bin-linux-x64.tar.gz',
       torrent: 'https://downloads.skycoin.net/wallet/skycoin-0.20.2.torrent',
       filetype: '.tar.gz',
-      filesize: '8.1mb',
+      filesize: '14mb',
     }],
   }],
 }];
