243e753ef194c69b0c9d1b0880c3f5bff266a238
gz-c <token@protonmail.com>
2017-10-20 00:16:03 -0500

    Remove .sketch brand guidelines (#129)

--- src/components/Downloads/components/Assets/content.js
+++ src/components/Downloads/components/Assets/content.js
@@ -4,11 +4,6 @@ export default [{
   filetype: '.pdf',
   filesize: '2.7mb',
 }, {
-  name: 'Skycoin Brand Guidelines [sketch]',
-  download: 'https://downloads.skycoin.net/branding/Skycoin-Brand_Guidelines-v1.4.sketch',
-  filetype: '.sketch',
-  filesize: '2.9mb',
-}, {
   name: 'Skycoin Logo PNGs',
   download: 'https://downloads.skycoin.net/branding/Skycoin-Logo-PNGs.zip',
   filetype: '.zip',
