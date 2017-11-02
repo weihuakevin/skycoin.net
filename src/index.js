7b85ca5b734a58b05176f4c8d865877fc807a049
coinyawong <30748399+coinyawong@users.noreply.github.com>
2017-11-02 11:56:42 +0900

    Update index.js (#137)
    * Update index.js
    
    Add 'kr'
    
    * Update index.js
    
    Why did it failed?
    
    * Update index.js
    
    again
    
    * Update index.js
    
    fixed

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { render } from 'react-snapshot';
 
 import 'moment/locale/ru';
 import 'moment/locale/zh-cn';
+import 'moment/locale/ko';
 import 'normalize.css/normalize.css';
 import './index.css';
 
