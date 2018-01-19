34637988808d7398a8222b94bd428470d7b26b7e
MatjazKotnik <matjaz@moonwalker7.com>
2018-01-19 10:33:02 +0100

    typo fix

--- src/components/Downloads/components/skywire/content.js
+++ src/components/Downloads/components/skywire/content.js
@@ -1,5 +1,5 @@
 export default [{
-  name: 'Skywire Miner Assebly Manual [pdf]',
+  name: 'Skywire Miner Assembly Manual [pdf]',
   download: 'https://downloads.skycoin.net/skywire/Skycoin+Miner+Assembly+Manual_Final_180108.pdf',
   filetype: '.pdf',
   filesize: '28.4mb',
